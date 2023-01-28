48396ffc72b6db7027b135bfec3826f966cd1eb2
Paula Kwakernaak <85444225+PaulaKwakernaak@users.noreply.github.com>
2023-01-28 22:12:05 +0100

    #we can ea bananas

--- BlazorBattle/Program.cs
+++ BlazorBattle/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBattle;
+using BlazorBattle.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IBananaService, BananaService>();
+
 
 await builder.Build().RunAsync();
---
+++ BlazorBattle/Services/IBananaService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorBattle.Services
+{
+    public interface IBananaService
+    {
+        event Action OnChange;
+        int Bananas { get; set; }
+
+        void EatBananas(int amount);
+
+        void AddBananas(int amount);
+    }
+}
